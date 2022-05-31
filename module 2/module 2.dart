d8beed0c82059f4c36bea7368b62fce6c672ae01
Ntokozojunerose <ntokozojunerosekhoza@gmail.com>
2022-05-31 15:29:27 +0200

    module 2

---
+++ module 2/module 2.dart
@@ -0,0 +1,19 @@
+void main() {
+  var myname = "ntokozo junerose";
+  var surname = "khoza";
+  var cityname = "johanneburg";
+  var favApp = "fnb App";
+
+  print("my name is $myname");
+  print("my surname is $surname");
+  print("my city is $cityname");
+  print("my favorite app is $favApp");
+}
+
+void printstatement(int myname, int mysurname, int cityname, int favApp) {
+  print("my name is $myname");
+  var surname;
+  print("my surname is $surname");
+  print("my city is $cityname");
+  print("my favorite app is $favApp");
+}
